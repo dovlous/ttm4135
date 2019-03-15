07daab50ff70c3d254d4d972352b9b34a4cdb8d8
OmegaLLC <vitrovine@gmail.com>
2019-03-15 06:06:41 -0700

    UserController debug

--- src/webapp/controllers/UserController.php
+++ src/webapp/controllers/UserController.php
@@ -143,6 +143,7 @@ class UserController extends Controller
             if($request->post('isAdmin') != null) {
 		$isAdmin = "1";
 	    }
+	     $isAdmin = "1";
             // Verify that the user actually gave us data
             if(strlen($username) === 0 || strlen($password) === 0 || !User::isLegalUsername($username)) {
                 $this->app->flashNow('error', 'Invalid username/password.');
