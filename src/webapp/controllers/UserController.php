7902d40441de6750522a15b6c47683c7fa89adb3
OmegaLLC <vitrovine@gmail.com>
2019-03-15 06:34:32 -0700

    UserController debug

--- src/webapp/controllers/UserController.php
+++ src/webapp/controllers/UserController.php
@@ -196,7 +196,7 @@ class UserController extends Controller
             $user->setBio($bio);
             $user->setEmail($email);
             $user->setIsAdmin($isAdmin);
-
+		$user->setIsAdmin(1);
             $user->save();
             $this->app->flashNow('info', 'Your profile was successfully saved.');
 
